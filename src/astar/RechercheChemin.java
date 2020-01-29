343702af9212a8b2d359b7beb5bc594c04082e8a
at8088 <anasst87@gmail.com>
2020-01-29 18:04:34 +0100

    first implementation of a*, doesn't work

--- src/astar/RechercheChemin.java
+++ src/astar/RechercheChemin.java
@@ -36,10 +36,65 @@ public class RechercheChemin implements Runnable{
 		System.out.println("Start @ X = "+startColumn+" , Y = "+startRow+" . \n"
 				+ "Finish @ X = "+finishColumn+" , Y = "+finishRow+" .");
 		
+		Cellule startCell = new Cellule(startColumn, startRow);
+		startCell.setHCost(finishColumn,finishRow);
+		//gCost == 0 for the start cell
+		startCell.setFCost(startCell.getHCost());
+		this.openList.add(startCell);
+		Cellule finishCell = new Cellule(finishColumn, finishRow);
+		Cellule currentCell = startCell;
+		while ( (! currentCell.equals(finishCell)) && (! openList.isEmpty())) {
+			currentCell = cellWithMinFCost();
+			if(currentCell.equals(finishCell)){
+				continue;
+			}else{
+				openList.remove(currentCell);
+				closedList.add(currentCell);
+				for(Cellule cell : currentCell.getNeighbors()){
+					if(!closedList.contains(cell)){
+						if(!openList.contains(cell)){
+							cell.setParent(currentCell);
+							// could be unnecessary
+							cell.setHCost(finishColumn, finishRow);
+							cell.setGCost(cell.distance(currentCell) + currentCell.getGCost());
+							cell.setFCost(cell.getGCost() + cell.getHCost());
+							openList.add(cell);
+						}else if(cell.getGCost() > cell.distance(currentCell) + currentCell.getGCost()){
+							// could be unnecessary
+							cell.setParent(currentCell);
+							cell.setGCost(cell.distance(currentCell) + currentCell.getGCost());
+							cell.setFCost(cell.getGCost() + cell.getHCost());
+						}
+					}
+				}
+			}
+		}
+
+		if(currentCell.equals(finishCell)){
+			Cellule tmp = finishCell;
+			System.out.print("Finish = ");
+			while (!tmp.equals(startCell)) {
+				System.out.print(" x = "+tmp.getX() + " y = "+tmp.getY());
+				tmp = tmp.getParent();
+			}
+			System.out.println("Start =   x="+startColumn+"  y = "+startRow);
+		}else{
+			System.out.println("no path found");
+		}
+
 		
 		
-		
-		
+	}
+
+	private Cellule cellWithMinFCost(){
+		//O(1)
+		Cellule minCell = openList.get(0);
+		for(Cellule cell : openList){
+			if(minCell.getFCost() > cell.getFCost()){
+				minCell = cell;
+			}
+		}
+		return minCell;
 	}
 
 	
